e5be8b92742b8fa7f892274d413504fe822e203a
debd1987 <deb.d786@gmail.com>
2021-10-23 14:15:14 +0530

    initial spring boot project

---
+++ src/main/java/com/example/demo/SsMicroservice1Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SsMicroservice1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SsMicroservice1Application.class, args);
+	}
+
+}
