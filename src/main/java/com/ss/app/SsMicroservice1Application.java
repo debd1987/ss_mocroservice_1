6afbde675bb232d32d0edf1a273e82a276dd1e13
debd1987 <deb.d786@gmail.com>
2021-10-25 10:30:32 +0530

    Adding files for creating first welcome controller

--- src/main/java/com/ss/app/SsMicroservice1Application.java
+++ src/main/java/com/ss/app/SsMicroservice1Application.java
@@ -2,11 +2,17 @@ package com.ss.app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.data.mongo.MongoDataAutoConfiguration;
+import org.springframework.boot.autoconfigure.mongo.MongoAutoConfiguration;
 
-@SpringBootApplication
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@SpringBootApplication(exclude = { MongoAutoConfiguration.class, MongoDataAutoConfiguration.class })
 public class SsMicroservice1Application {
 
-	public static void main(String[] args) {
+	public static void main(final String[] args) {
+		log.info("Starting application SsMicroservice {} ");
 		SpringApplication.run(SsMicroservice1Application.class, args);
 	}
 
---
+++ src/main/java/com/ss/app/controller/WelcomeController.java
@@ -0,0 +1,21 @@
+package com.ss.app.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@RestController
+@RequestMapping("/apis/v1/ss")
+public class WelcomeController {
+
+	@GetMapping("/welcome")
+	public ResponseEntity<String> getHelloWorld() {
+		log.info("Received call within welcome controller {}");
+		return ResponseEntity.ok("Welcome to System Service");
+	}
+
+}
